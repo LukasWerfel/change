15f4b208d096b6d3d89dd3b93b0543da45ef3089
Lukas Werfel <hi@lukaswerfel.com>
2019-01-27 13:49:50 +0100

    Add some ugly basic journal entries

--- src/App/components/Content/components/JournalList/components/Journal/Journal.tsx
+++ src/App/components/Content/components/JournalList/components/Journal/Journal.tsx
@@ -1,5 +1,42 @@
 import React from "react"
+import styled from "@emotion/styled"
+import { COLOR, BASE_SIZING_UNIT } from "../../../../../../variables"
 
-const Journal = () => <div>Create OSS PR</div>
+const Entry = styled.div({
+  height: `${3 * BASE_SIZING_UNIT}px`,
+  boxShadow: "inset 0 0 1px #000000",
+  flex: 1,
+  ":first-of-type": {
+    borderTopLeftRadius: "4px",
+    borderBottomLeftRadius: "4px",
+  },
+  ":last-child": {
+    borderTopRightRadius: "4px",
+    borderBottomRightRadius: "4px",
+  },
+})
+
+const EntryList = styled.div({
+  paddingTop: `${1 * BASE_SIZING_UNIT}px`,
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "center",
+})
+
+const Journal = () => (
+  <div>
+    <span>Create OSS PR</span>
+    <EntryList>
+      <Entry />
+      <Entry />
+      <Entry />
+      <Entry />
+      <Entry />
+      <Entry />
+      <Entry />
+    </EntryList>
+  </div>
+)
 
 export default Journal
