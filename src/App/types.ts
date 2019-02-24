2b04d6e170feb42f3dacaf339f851e4fffa858f1
Lukas Werfel <hi@lukaswerfel.com>
2019-02-24 13:45:10 +0100

    Add Entry type

--- src/App/types.ts
+++ src/App/types.ts
@@ -4,12 +4,27 @@ import aguid from "aguid"
 /* prettier breaks this code somehow */
 /* TODO: investigate */
 // prettier-ignore
+export type Entry = {
+  date: string,
+  status: "SUCCEEDED" | "FAILED" | "SKIPPED",
+}
+
+export const entryFactory = Factory.Sync.makeFactory<Entry>({
+  date: Factory.each(i => new Date().toISOString().slice(0, 10)),
+  status: "SUCCEEDED",
+})
+
+/* prettier breaks this code somehow */
+/* TODO: investigate */
+// prettier-ignore
 export type Journal = {
   id: string,
   name: string,
+  entries: Array<Entry>,
 }
 
 export const journalFactory = Factory.Sync.makeFactory<Journal>({
   id: Factory.each(i => aguid(`${i}`)),
   name: "create oss pr",
+  entries: [],
 })
